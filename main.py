c74a8492e75ca610510fce9291d6794b5bd2472d
Hamid Alavifar <101734217+Hamid-Alavifar@users.noreply.github.com>
2024-05-24 12:45:01 +0330

    Update main.py
    added glass amount calculation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 x = input ( "Enter your weight (in KG): ")
 x = float (x)
 y = (x*3.1249962835)/100
-print("You Should Drink", round(y, 2), "Liters of Water each day")
+print("You need to drink at least [", round(y, 2), "] liters or about [", round((y*1000/250),1), " glasses of water daily")
